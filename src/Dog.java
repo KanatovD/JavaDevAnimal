681315af0920651337b9814671779df2a4e3fec2
temaprof <kanatovd55@mail.com>
2024-01-29 19:27:46 +0600

    add method Dog is eating

--- src/Dog.java
+++ src/Dog.java
@@ -3,5 +3,8 @@ public class Dog extends Animal {
     public void sleep(){
         System.out.println("Dog is sleeping");
     }
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
 
-}
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        animal.sleep();
+        Dog dog = new Dog();
+        dog.eat();
     }
 }
